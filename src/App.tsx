593ef7cdf82d6e169a9c4234b3417341e1424d0d
heusser-dev <heusser.escamilla@gmail.com>
2024-02-11 17:17:19 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import Navbar from './components/Header/navbar';
+import Template from './components/modelo3d/modelo3d';
+import { Card } from 'antd';
+import Modelo3d from './components/modelo3d/modelo3d';
+import ComponenteConModelo3D from './components/Card/card';
+const { Header, Content, Footer } = Layout;
 
-function App() {
+const { Meta } = Card;
+
+const items = new Array(3).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}));
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header className="sticky top-0 z-10 w-full flex items-center bg-sky-900 text-white p-4">
+      <Navbar></Navbar>
+        
+      </Header>
+      <ComponenteConModelo3D/>
+      <Footer className="text-center">
+        Ant Design ©{new Date().getFullYear()} Created by heusser-dev
+      </Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+
+    <nav className="sticky top-0 z-10 mx-auto h-20 bg-cyan-600 text-white p-2 rounded-b-lg">
+      {/* Contenido del Navbar */}
+      <div className="container mx-4">
+        <div className="flex justify-between items-center">
+          <ul className="flex space-x-6">
+            <li><a href="#" className="hover:text-yellow-300 text-2xl">Inicio</a></li>
+            <li><a href="#" className="hover:text-yellow-300 text-2xl">Acerca</a></li>
+            <li><a href="#" className="hover:text-yellow-300 text-2xl">Servicios</a></li>
+            <li><a href="#" className="hover:text-yellow-300 text-2xl">Contacto</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
